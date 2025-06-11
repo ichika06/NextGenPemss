e8bddfe39a6acb6532a91361228a49db22b23bea
ichika06 <jmestareja05@gmail.com>
2025-06-11 21:08:50 +0800

    fixed relative on modal

--- src/pages/StreamEvent.jsx
+++ src/pages/StreamEvent.jsx
@@ -393,7 +393,7 @@ export default function LiveEventsList() {
               ></div>
 
               {/* Modal panel */}
-              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border border-gray-300">
+              <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border border-gray-300">
                 <div className="bg-white px-6 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
